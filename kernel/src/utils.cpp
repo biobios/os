13184b08c01aeaff0a564a2f7636b729c2736408
BioBIOS <biobios1024@gmail.com>
2023-06-08 16:51:36 +0900

    def function [number to hex]

---
+++ kernel/src/utils.cpp
@@ -0,0 +1,17 @@
+#include "utils.hpp"
+
+void oz::utils::to_hex(std::uint64_t num, char* str) {
+    std::uint64_t i;
+    std::uint64_t halfByte;
+    for(i = 0; i < 2*sizeof(num); i++){
+        halfByte = num & 0x000000000000000F;
+        if(halfByte >= 10){
+            str[i] = ('A' + halfByte - 0xA);
+        }else{
+            str[i] = ('0' + halfByte);
+        }
+        num = num >> 4;
+    }
+
+    str[2*sizeof(num)] = '\0';
+}
