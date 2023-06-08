55654b61511b65616c9e62125022a666048749d9
BioBIOS <biobios1024@gmail.com>
2023-06-08 16:54:30 +0900

    fix function[number to hex]

--- kernel/src/utils.cpp
+++ kernel/src/utils.cpp
@@ -1,9 +1,9 @@
 #include "utils.hpp"
 
 void oz::utils::to_hex(std::uint64_t num, char* str) {
-    std::uint64_t i;
+    std::int64_t i;
     std::uint64_t halfByte;
-    for(i = 0; i < 2*sizeof(num); i++){
+    for(i = 2*sizeof(num) - 1; i >= 0; i--){
         halfByte = num & 0x000000000000000F;
         if(halfByte >= 10){
             str[i] = ('A' + halfByte - 0xA);
