2cc37d56ff131871ceeefccc1f31c451cdae0fc5
BioBIOS <biobios1024@gmail.com>
2023-06-09 23:37:24 +0900

    align kernel, and kernelPtr to public.

--- kernel/src/main.cpp
+++ kernel/src/main.cpp
@@ -2,9 +2,10 @@
 #include <cstdint>
 #include <new>
 
-std::uint8_t k[sizeof(oz::Kernel)];
+alignas(oz::Kernel) std::uint8_t k[sizeof(oz::Kernel)];
 
 extern "C" void main(PlatformInfo* platformInfo) {
+    setKernelPtr(static_cast<void*>(&k));
     new(static_cast<void*>(&k)) oz::Kernel{platformInfo};
     reinterpret_cast<oz::Kernel*>(&k)->run();
     while (1) __asm__ volatile("hlt");
